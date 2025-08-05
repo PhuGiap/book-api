7a84e259d6f661696d4ae636120cc057a1d4d746
PhuGiap <giapphutrinh2018@gmail.com>
2025-08-05 19:32:23 +0700

    remove folder helper

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ exports.getAllUsers = async (req, res) => {
     res.json(users);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server errorrrr' });
+    res.status(500).json({ message: 'Server error' });
   }
 };
 
